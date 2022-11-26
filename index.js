5da46ef0b9cda4bdbd5d75d7de2755e991475a3d
unknown <parvezmrf@gmail.com>
2022-11-26 09:32:52 +0600

    server update

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+
+const app = express();
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://<username>:<password>@cluster0.wgjsrij.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+
+
+
+
+app.get('/', async (req, res) => {
+    res.send('Retail Mart server is running')
+})
+
+app.listen(port, () => console.log(`Retail Mart running on ${port}`))
