0e320ec57d5aeb2775bc1a9ee46167dc341a293d
unknown <parvezmrf@gmail.com>
2022-11-26 10:11:11 +0600

    db conected

--- index.js
+++ index.js
@@ -15,7 +15,22 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wgjsrij.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        const productsCollection = client.db('retailsMart').collection('products');
 
+        app.get('/products', async (req, res) => {
+            const query = {}
+            const getproducts = await productsCollection.find(query).toArray();
+            res.send(getproducts)
+        })
+    }
+    finally {
+
+    }
+
+}
+run().catch(console.log)
 
 
 
