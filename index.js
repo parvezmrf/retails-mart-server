8e0649cc32f37aa99a4d322ee11258b965ceb394
unknown <parvezmrf@gmail.com>
2022-11-26 09:34:32 +0600

    mongo client import

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://<username>:<password>@cluster0.wgjsrij.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
