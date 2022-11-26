f1856eb4b43b7717a7eaab364e957af20dffcfc6
unknown <parvezmrf@gmail.com>
2022-11-26 09:38:34 +0600

    env create

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.use(cors())
 app.use(express.json())
 
 
-
 const uri = "mongodb+srv://<username>:<password>@cluster0.wgjsrij.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
