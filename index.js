85ed5537b7a4cac5e0081b5290dd926ab7a180f6
unknown <parvezmrf@gmail.com>
2022-11-26 09:42:09 +0600

    db user pass set

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.wgjsrij.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wgjsrij.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
